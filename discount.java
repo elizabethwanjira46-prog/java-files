b4b884df4643e7ec00b86f05ae3bc9754467d4bf
elizabethwanjira46-prog <elizabethwanjira46@gmail.com>
2025-09-24 12:54:38 +0300

    Create discount.java

---
+++ discount.java
@@ -0,0 +1,26 @@
+//Program to calculate discountI
+//NAME: Elizabeth Wanjira
+//REG NO:S08-8408-2024
+import java.util.Scanner;
+public class Discount{
+	public static void main(String[] args) {
+		Scanner Scanner=new Scanner (System.in);
+		double discount;
+		System.out.println("Amount purchased:");
+		double amount=Scanner.nextDouble();
+		if(amount>5000){
+			discount=amount*0.1;
+		}
+		else if(amount>=1000&& amount<=5000){
+			discount=0.05*amount;
+		}
+		else{
+			discount=0;
+		}
+		double amountTopay=amount-discount;
+		System.out.println("initial amount"+amount);
+		System.out.println("discount"+discount);
+		System.out.println("amount to pay"+amountTopay);
+		Scanner.close();
+	}
+}
